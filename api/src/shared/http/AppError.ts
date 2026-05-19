819a760c4ff8e6fce6de6f747f7652da5ace7ea2
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2026-05-19 10:16:17 -0400

    fix: add missing Details type to AppError

--- api/src/shared/http/AppError.ts
+++ api/src/shared/http/AppError.ts
@@ -1,10 +1,11 @@
+import { Details } from "./response.types";
 
 export class AppError extends Error {
     constructor(
         public statusCode: number,
         public code: string,
         public message: string,
-        public details?: unknown // /definir tipo do details
+        public details?: Details // /definir tipo do details
     ) {
         super(message);
     }
