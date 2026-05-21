f01c3dd0858f6ddb483ddb7aba510491a3b7374f
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2026-05-21 14:58:04 -0400

    chore: scaffold feature architecture and initial routes

---
+++ api/src/modules/contracts/contracts.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+const contractsRouter = Router();
+
+// list many
+
+export default contractsRouter;
--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import customersRoutes from "@/modules/customers/customers.routes";
+import contractsRouter from "@/modules/contracts/contracts.routes";
 
 const router = Router();
 
 
 // routes
 router.use("/customers", customersRoutes);
+router.use("/contracts", contractsRouter);
 
 router.get('/health', (req, res) => {
     res.status(200).json({
