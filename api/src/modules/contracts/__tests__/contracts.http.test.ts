180cd3c9a06b8cf5c13e5352415328d9ea0582a2
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2026-05-26 14:51:44 -0400

    test: implement HTTP tesr for /api/contracts/:id PUT 200

--- api/src/modules/contracts/__tests__/contracts.http.test.ts
+++ api/src/modules/contracts/__tests__/contracts.http.test.ts
@@ -59,4 +59,44 @@ describe("Contracts HTTP Tests", () => {
             contractDataApiResponseSchema.parse(res.body);
         });
     });
+
+    describe("PUT /api/contracts/:id (status) - 200", () => {
+        it("should update a contract", async () => {
+            const oldContract: CreateContractInputBody = {
+                customerId: "6e25d18d-2862-4b76-9ac9-54dc7791638d",
+                title: "old title",
+                totalAmount: "123234",
+                installmentCount: 21,
+                installmentFrequency: "BIWEEKLY",
+                interestRate: "21",
+                interestPeriod: "ANNUALLY",
+                startDate: "2026-09-05T00:00:00.000Z",
+                description: "old descrição",
+                skipSaturday: true,
+                skipSunday: true
+            };
+
+            const createRes = await request(app).post("/api/contracts").send(oldContract);
+
+            const id = createRes.body.data.id;
+            const updateData:CreateContractInputBody = {
+                customerId: "6e25d18d-2862-4b76-9ac9-54dc7791638d",
+                title: "new title",
+                totalAmount: "123234",
+                installmentCount: 45,
+                installmentFrequency: "MONTHLY",
+                interestRate: "20",
+                interestPeriod: "MONTHLY",
+                startDate: "2026-09-05T00:00:00.000Z",
+                description: "new descrição",
+                skipSaturday: false,
+                skipSunday: false
+            };
+
+            const res = await request(app).put(`/api/contracts/${id}`).send(updateData);
+
+            expect(res.status).toBe(200);
+            contractDataApiResponseSchema.parse(res.body);
+        });
+    });     
 })
