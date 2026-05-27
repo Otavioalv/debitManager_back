b5bcf39277d9f7d5d3c38fcd4ceb8f4ffd156a70
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2026-05-27 14:22:49 -0400

    add installments model to schema

---
+++ api/src/modules/installments/intallments.repository.ts
@@ -0,0 +1,7 @@
+
+
+export class InstallmentsRepository {
+    public async createManyInstallments(/* db: DbClient, data: CreateInstallmentBody[] */)/* : Promise<Installment[]> */ {
+        console.log("Creating installments... (This is a placeholder function, implement the actual database logic here)");
+    }
+}
